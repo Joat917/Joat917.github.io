15ecc04efc7111d22a68fe2b1d95d6a7a7909cea
Joat917 <2778561258@qq.com>
2026-09-20 20:35:28 +0800

    重写导航页，现在不跳转到旧有仓库了。

---
+++ links.js
@@ -0,0 +1,17 @@
+const LINKS = [
+  { name: '114514', url: '/HTMGames/homo.html', icon: '', desc: 'Decomposite every number into 1145141919810 とても面白いですね' },
+  { name: 'Ising', url: '/HTMGames/ising.html', icon: '', desc: 'Ising model simulation' },
+
+  { name: 'PKU-GPA', url: '/HTMGames/gpa.html', icon: '', desc: 'GPA calculator for Peking University (deprecated after GPA reform)' },
+  { name: 'PKU-Elective', url: '/HTMGames/electivePoints.html', icon: '', desc: 'Elective points calculator for Peking University students' },
+
+  { name: 'Minesweeper', url: '/Minesweeper4/', icon: '', desc: 'Minesweeper game' },
+  { name: 'SudoKu', url: '/Sudoku4/', icon: '', desc: 'Sudoku game' },
+  { name: 'Bingo', url: '/Bingo4/', icon: '', desc: 'Bingo game' },
+  { name: 'Digital Klotski', url: '/Klotski4/', icon: '', desc: 'Digital Klotski game' },
+  { name: 'Sokoban Solver', url: '/SokobanSolver/', icon: '', desc: 'Sokoban game solver' },
+  { name: 'Snake', url: '/HTMGames/snake_path.htm', icon: '', desc: 'Snake game' },
+  { name: 'DrawCircle', url: '/DrawCircle/', icon: '', desc: 'Draw a circle' },
+
+  { name: 'NavOld', url: '/HTMGames/', icon: '', desc: 'Old navigation page' },
+];
